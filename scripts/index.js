c5f91ac02b45456db523f3b07c47bd4d60749f17
Vlad Kurmaev <Vladikurmav@yandex.ru>
2021-06-16 17:25:33 +0300

    feat: script popup

--- scripts/index.js
+++ scripts/index.js
@@ -2,11 +2,46 @@ const popupProfileRedactElement = document.querySelector('.profile__redact');
 const popupElement = document.querySelector('.popup');
 const popupExitButtonElement = popupElement.querySelector('.popup__exit');
 
+const popupNameInputElement = popupElement.querySelector('.popup__input-name');
+const popupDescriptionInputElement = popupElement.querySelector('.popup__input-description');
+const nameElement = document.querySelector('.profile__name');
+const descriptionElement = document.querySelector('.profile__description');
+const popupSaveElement = popupElement.querySelector('.popup__save');
+
+const nameAndDescriptionLikeHTML = function() {
+popupNameInputElement.value = nameElement.textContent;
+popupDescriptionInputElement.value = descriptionElement.textContent;
+};
+
 const openPopup = function() {
-  popupElement.classList.add('popup_is-opened')
+  nameAndDescriptionLikeHTML();
+  popupElement.classList.add('popup_is-opened');
 };
 const closePopup = function() {
-  popupElement.classList.remove('popup_is-opened')
+  nameAndDescriptionLikeHTML();
+  popupElement.classList.remove('popup_is-opened');
 };
 popupProfileRedactElement.addEventListener('click', openPopup);
 popupExitButtonElement.addEventListener('click', closePopup);
+
+const saveNameAndDescription = function(evt) {
+  evt.preventDefault();
+  nameElement.textContent = popupNameInputElement.value;
+  descriptionElement.textContent = popupDescriptionInputElement.value;
+  closePopup ()
+};
+popupSaveElement.addEventListener('click', saveNameAndDescription);
+popupSaveElement.addEventListener('submit', saveNameAndDescription);
+
+popupNameInputElement.addEventListener("keyup", function(event) {
+  if (event.keyCode === 13) {
+    nameElement.textContent = popupNameInputElement.value;
+    closePopup ();
+  }
+});
+popupDescriptionInputElement.addEventListener("keyup", function(event) {
+  if (event.keyCode === 13) {
+    descriptionElement.textContent = popupDescriptionInputElement.value;
+    closePopup ();
+  }
+});
