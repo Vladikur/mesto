afd9cf1c00b3bc5a046062286e1e4e34a20b1a13
Vlad Kurmaev <Vladikurmav@yandex.ru>
2021-06-17 17:23:57 +0300

    bug fix3

--- scripts/index.js
+++ scripts/index.js
@@ -2,8 +2,8 @@ const popupProfileRedactElement = document.querySelector('.profile__redact');
 const popupElement = document.querySelector('.popup');
 const popupExitButtonElement = popupElement.querySelector('.popup__exit');
 
-const popupNameInputElement = popupElement.querySelector('.popup__input_name');
-const popupDescriptionInputElement = popupElement.querySelector('.popup__input_description');
+const popupNameInputElement = popupElement.querySelector('.popup__input_change_name');
+const popupDescriptionInputElement = popupElement.querySelector('.popup__input_change_description');
 const nameElement = document.querySelector('.profile__name');
 const descriptionElement = document.querySelector('.profile__description');
 const popupFormElement = popupElement.querySelector('.popup__form');
