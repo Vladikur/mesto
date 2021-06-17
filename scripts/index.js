4cdeb190c4c5fc26eda744cc51a49ce951e8a27b
Vlad Kurmaev <Vladikurmav@yandex.ru>
2021-06-17 14:58:15 +0300

    bug fix2

--- scripts/index.js
+++ scripts/index.js
@@ -2,15 +2,15 @@ const popupProfileRedactElement = document.querySelector('.profile__redact');
 const popupElement = document.querySelector('.popup');
 const popupExitButtonElement = popupElement.querySelector('.popup__exit');
 
-const popupNameInputElement = popupElement.querySelector('.popup__input-name');
-const popupDescriptionInputElement = popupElement.querySelector('.popup__input-description');
+const popupNameInputElement = popupElement.querySelector('.popup__input_name');
+const popupDescriptionInputElement = popupElement.querySelector('.popup__input_description');
 const nameElement = document.querySelector('.profile__name');
 const descriptionElement = document.querySelector('.profile__description');
-const popupSaveElement = popupElement.querySelector('.popup__save');
+const popupFormElement = popupElement.querySelector('.popup__form');
 
 const nameAndDescriptionLikeHTML = function() {
-popupNameInputElement.value = nameElement.textContent;
-popupDescriptionInputElement.value = descriptionElement.textContent;
+  popupNameInputElement.value = nameElement.textContent;
+  popupDescriptionInputElement.value = descriptionElement.textContent;
 };
 
 const openPopup = function() {
@@ -18,11 +18,8 @@ const openPopup = function() {
   popupElement.classList.add('popup_is-opened');
 };
 const closePopup = function() {
-  nameAndDescriptionLikeHTML();
   popupElement.classList.remove('popup_is-opened');
 };
-popupProfileRedactElement.addEventListener('click', openPopup);
-popupExitButtonElement.addEventListener('click', closePopup);
 
 const saveNameAndDescription = function(evt) {
   evt.preventDefault();
@@ -30,18 +27,8 @@ const saveNameAndDescription = function(evt) {
   descriptionElement.textContent = popupDescriptionInputElement.value;
   closePopup ()
 };
-popupSaveElement.addEventListener('click', saveNameAndDescription);
-popupSaveElement.addEventListener('submit', saveNameAndDescription);
 
-popupNameInputElement.addEventListener("keyup", function(event) {
-  if (event.keyCode === 13) {
-    nameElement.textContent = popupNameInputElement.value;
-    closePopup ();
-  }
-});
-popupDescriptionInputElement.addEventListener("keyup", function(event) {
-  if (event.keyCode === 13) {
-    descriptionElement.textContent = popupDescriptionInputElement.value;
-    closePopup ();
-  }
-});
+popupProfileRedactElement.addEventListener('click', openPopup);
+popupExitButtonElement.addEventListener('click', closePopup);
+popupFormElement.addEventListener('submit', saveNameAndDescription);
+
